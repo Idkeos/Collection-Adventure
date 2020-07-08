bfb6ba739f6760ff2c1b6f276115303e0fa74a6b
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2020-07-09 00:14:40 +0300

    Added basic movement for player

---
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 20f;
+    public float TurnSpeed = 300f;
+
+    private float MovementInputValue;
+    private float TurnInputValue;
+
+    private Rigidbody Rigidbody;
+
+    private void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        MovementInputValue = Input.GetAxis("Vertical");
+        TurnInputValue = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Turn();
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = transform.forward * MovementInputValue * Speed * Time.deltaTime;
+
+        Rigidbody.MovePosition(Rigidbody.position + movement);
+    }
+
+    private void Turn()
+    {
+        float turn = TurnInputValue * TurnSpeed * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        Rigidbody.MoveRotation(Rigidbody.rotation * turnRotation);
+    }
+}
