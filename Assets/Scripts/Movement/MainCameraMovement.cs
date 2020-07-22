346d0491aa5afd3f96f17affe37a735d1ca56a34
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2020-07-22 19:05:54 +0300

    Added camera rotation

---
+++ Assets/Scripts/Movement/MainCameraMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraMovement : MonoBehaviour
+{
+    public float TurnSpeed = 200f;
+    private float TurnInputValue;
+
+    private Vector3 LocalStartPos;
+    private Quaternion LocalStartRot;
+
+    private void Start()
+    {
+        LocalStartPos = gameObject.transform.localPosition;
+        LocalStartRot = gameObject.transform.localRotation;
+    }
+
+    private void Update()
+    {
+        TurnInputValue = Input.GetAxis("Horizontal Camera");
+    }
+
+    private void LateUpdate()
+    {   
+        RoateCamera();
+
+        if (Input.GetButtonDown("Reset Camera")) {
+            ResetCamera();
+        }
+    }
+
+    private void RoateCamera()
+    {
+        transform.RotateAround(transform.parent.position, Vector3.up, -1 * TurnInputValue * TurnSpeed * Time.deltaTime);
+    }
+
+    private void ResetCamera()
+    {
+        transform.localPosition = LocalStartPos;
+        transform.localRotation = LocalStartRot;
+    }
+}
